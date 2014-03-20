1cf9ca17a2bed9ff12cf4b01e3b33c9b44518f36
rice2007 <rice2007@gmail.com>
2014-03-20 02:07:12 -0500

    Complete4

--- ricea4.asm
+++ ricea4.asm
@@ -10,80 +10,78 @@ ascedended order.
 include Irvine32.inc
 .data
 blank    BYTE      " ",0ah,0
-lsbMsg   BYTE      "LSB in postition ",0
-msbMsg   BYTE      "MSB in postition ",0
-prompt   BYTE      "Input a hex value: ",0
-ttlMsg   BYTE      "Total set bits: ",0
-zeroMsg  BYTE      "No bits set",0ah,0
-bitmask  SDWORD    1h
-LSB      SDWORD    20h
-MSB      SDWORD    20h
+oFlow    BYTE      "Array overflow ",0ah,0
+prompt   BYTE      "Input an integer value: ",0
+space    BYTE      " ",0
+uFlow    BYTE      "Array underflow.",0ah,0
+zErr     BYTE      "No array entries found",0ah,0
+arr      SDWORD    100 dup(?)
+len      SDWORD    ?
+lenMO    SDWORD    ?
 
 .code
 main PROC
 
-         mov       edx,offset prompt   
-         call      writeString         ;Prompt user for input
-         call      readHex             ;Read input from user
-         mov       ecx,32              ;Clear ecx for iteration ctr
-         mov       edx,0               ;Clear edx for bit ctr
+         mov       esi,offset arr      ;Set esi to mem location of arr
+         mov       ebx,0               ;Clear ebx to count elements
+         mov       ecx,100             ;Set ecx to ensure arr.len < 101
 
-         cmp       eax,0
-         jz        zroBit
-lsbCt:   test      eax,bitmask         ;Test status of right-most bit
-         .if       !zero?              ;If right-most bit is set...
-          inc      edx                 ;Increase total bits ctr
-          sub      LSB,ecx             ;Store least-sig bit in LSB
-          mov      ebx,ecx             ;Track last instance of set bit
-          dec      ecx                 ;Dec iterator (no dec from loop)
-          jmp      msbCt               ;Jump when LSB is found
+input:   mov       edx,offset prompt   ;Prompt user for input
+         call      writeString         
+         call      readInt             ;Read input from user
+         cmp       eax,0               ;Test for terminal 0
+         jz        clrReg              ;Jump to sorting on terminal 0
+         mov       [esi],eax           ;Add input to array
+         add       esi,4               ;Move to next array address
+         inc       ebx                 ;Incriment element counter
+         loop      input               ;Continue receiving input
+
+clrReg:  cmp       ebx,0               ;Test if arr is empty
+         jz        err                 ;Exit if arr is empty
+         mov       len,ebx             ;Store arr length in memory
+         dec       ebx                 
+         mov       lenMO,ebx           ;Store arr length -1
+         mov       ecx,-1              ;Use ecx as outer loop counter
+         mov       edx,ecx             
+         inc       edx                 ;Use edx as inner loop counter
+
+outLoop: inc       ecx                 ;Inc outer loop counter
+         mov       eax,ecx             ;Track min value of arr
+         mov       edx,ecx             ;Set inner loop counter
+         inc       edx                 ;Inc inner loop value
+
+inLoop:  cmp       edx,len             ;Test loop exit condition
+         jge       clrLoop             ;Jump on exit condition
+         mov       esi,arr[4*edx]      ;Use esi to compare val in arr
+         mov       edi,arr[4*eax]      ;Use edi to compare val in arr
+         .if       esi < edi           
+          mov      eax,edx             ;Move min val pointer if new min
          .endIf
-         shr       eax,1               ;Shift to test next bit
-         loop      lsbCt               ;Loop until all bits are tested
+         inc       edx                 ;Incease inner loop counter
+         cmp       edx,len             ;Test loop exit condition
+         jl        inLoop
 
-msbCt:   shr       eax,1               ;Shift to test next bit
-         test      eax,bitmask         ;Tests status of right-most bit
-         .if       !zero?              ;If right-most bit is set...
-          inc      edx                 ;Increase total bits ctr
-          mov      ebx,ecx             ;Track last instance of set bit
-         .endIf       
-         loop      msbCt               ;Loop until all bits are tested
+         .if       eax != ecx          ;MASM swap routine. Checks if 
+          mov      ebx,arr[4*ecx]      ; value is not in correct pos
+          xchg     ebx,arr[4*eax]      ; and swaps the two elements
+          xchg     ebx,arr[4*ecx]      ; if needed.
+         .endIf
+         cmp       ecx,lenMO           ;Test loop exit condition
+         jl        outLoop
 
-         sub       MSB,ebx             ;Store most-sig bit in MSB
-         mov       ebx,edx             ;Move total bit couont to ebx
-
-         comment ;
-Prints results to console in the order of least significant bit
-position, most significant bit position, and total set bits. Strings
-can only be passed to edx for output, and numerical values can only be
-passed to eax for output.
-;
-         mov       edx,offset lsbMsg
-         call      WriteString
-         mov       eax,LSB
+clrLoop: mov       ecx,0               ;Set ecx for memory scalar
+prtLoop: mov       eax,arr[4*ecx]      ;Put element in write register
          call      WriteInt
-         mov       edx,offset blank
+         mov       edx, offset space   ;Add space between output ints
          call      WriteString
+         inc       ecx                 ;Inc memory address scalar
+         cmp       ecx,len             ;Test for remaining elements
+         jl        prtLoop
+         invoke    exitProcess,0       ;Terminate gracefully
 
-         mov       edx,offset msbMsg
+err:     mov       edx,offset zErr     ;Print error msg
          call      WriteString
-         mov       eax,MSB
-         call      WriteInt
-         mov       edx,offset blank
-         call      WriteString
-
-         mov       edx,offset ttlMsg
-         call      WriteString
-         mov       eax,ebx
-         call      WriteInt
-         mov       edx,offset blank
-         call      WriteString
-         invoke    exitprocess,0       ;Terminate gracefully
-
-zroBit:  mov       edx,offset zeroMsg  
-         call      WriteString
-         invoke    exitprocess,0       ;Terminate gracefully
-
+         invoke    exitProcess,0       ;Terminate gracfully
 main ENDP
 
 END main
